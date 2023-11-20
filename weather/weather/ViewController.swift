34cecf7c6cfd721a53b72dc61021f0daf3d51c65
Tien Thanh <tienthanh@mac-mini.local>
2023-11-20 14:41:21 +0700

    second commit

---
+++ weather/weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Tien Thanh on 11/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
